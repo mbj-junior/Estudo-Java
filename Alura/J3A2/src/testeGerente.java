12bcd13110be383ef966158c200e0c70ad0ea8f4
Márcio Botelho Junior <mbj_junior@hotmail.com>
2020-06-10 12:05:55 -0300

    Aula 07 aplicando herança

---
+++ Alura/J3A2/src/testeGerente.java
@@ -0,0 +1,13 @@
+public class testeGerente {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setName("marcio");
+        g1.setCpf("123131322");
+        g1.setSalario(5000);
+
+        boolean autenticou = g1.autentica(1111);
+
+        System.out.println(g1.getName()+g1.getSalario()+g1.getCpf());
+        System.out.println(autenticou);
+    }
+}
