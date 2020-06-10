153977ff0117f56b42280ae77c2a023566647837
Márcio Botelho Junior <mbj_junior@hotmail.com>
2020-06-10 18:39:34 -0300

    Override

---
+++ Alura/Banco/src/PKDefault/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package PKDefault;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(int agencia, int numero){
+        super(agencia,numero);
+    }
+
+}
---
+++ Alura/Banco/src/PKDefault/TesteContas.java
@@ -0,0 +1,21 @@
+package PKDefault;
+
+public class TesteContas {
+
+	public static void main(String[] args) {
+
+		ContaCorrente cc1 = new ContaCorrente(1337, 23334);
+		cc1.deposita(200);
+
+		ContaPoupanca cp1 = new ContaPoupanca(2222,2222);
+		cp1.deposita(300);
+
+		System.out.println(cc1.getSaldo() + " " + cp1.getSaldo());
+
+		cc1.transfere(10,cp1);
+
+		System.out.println(cc1.getSaldo() + " " + cp1.getSaldo());
+		
+	}
+	
+}
