db217be3219657c7910f63b535cc64f5ea873c58
Márcio Botelho Junior <mbj_junior@hotmail.com>
2020-06-11 10:17:04 -0300

    conhecendo interface

---
+++ Alura/J3A2/src/Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente implements Autenticavel{
+
+    private int senha;
+
+    @Override
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean autentica(int senha) {
+        if (this.senha == senha){
+            System.out.println("senha valida");
+            return true;
+        } else {
+            System.out.println("senha invalida");
+            return false
+        }
+    }
+}
