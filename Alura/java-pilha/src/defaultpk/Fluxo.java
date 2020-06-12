91e0b6a44a0fe6a01ce89f40d74d01d058cbc3c4
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-12 17:55:21 -0300

    Usando o try catch

--- Alura/java-pilha/src/defaultpk/Fluxo.java
+++ Alura/java-pilha/src/defaultpk/Fluxo.java
@@ -10,7 +10,11 @@ public class Fluxo {
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+        try {
+            metodo2();
+        }catch (ArithmeticException ex){
+            System.out.printf("ArithmeticException");
+        }
         System.out.println("Fim do metodo1");
     }
 
@@ -18,6 +22,7 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            int a = i/0;
         }
         System.out.println("Fim do metodo2");
     }
