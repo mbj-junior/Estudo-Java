a0af382f4de8dfaf25ec08f261625f3106f3e657
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-12 19:15:30 -0300

    throw new ArithmeticException, criando uma exception

---
+++ Alura/java-pilha/src/defaultpk/FluxoComTratamento.java
@@ -0,0 +1,30 @@
+package defaultpk;
+
+public class FluxoComTratamento {
+
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        try {
+            metodo1();
+        }catch (ArithmeticException | NullPointerException ex){
+           String msg = ex.getMessage();
+           System.out.println("Error é "+msg);
+           ex.printStackTrace();
+        }
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+
+        throw new ArithmeticException ("Deu error");
+
+        //System.out.println("Fim do metodo2");
+    }
+}
