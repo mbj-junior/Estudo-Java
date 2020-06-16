11165e5bc750accacec359991791ec5571b4f6cc
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-16 19:10:28 -0300

    Usando biblioteca

---
+++ Alura/BancoImportado/src/br/com/home/bytebank/TesteBiblioteca.java
@@ -0,0 +1,14 @@
+package br.com.home.bytebank;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteBiblioteca {
+
+    public static void main(String[] args) {
+
+        Conta c = new ContaCorrente(122,122);
+
+    }
+
+}
