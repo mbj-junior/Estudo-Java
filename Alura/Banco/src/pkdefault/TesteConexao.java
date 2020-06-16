37db06879e71de407e155d42d2ea69b6193bd91e
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-16 08:27:34 -0300

    Aplicando o finally

---
+++ Alura/Banco/src/pkdefault/TesteConexao.java
@@ -0,0 +1,18 @@
+package pkdefault;
+
+public class TesteConexao {
+    public static void main(String[] args) {
+
+        Conexao con = null;
+        try {
+            con = new Conexao();
+            con.leDados();
+        }catch (IllegalStateException ex){
+            System.out.println("Error na conexão");
+        }finally {
+            con.fecha();
+        }
+
+
+    }
+}
