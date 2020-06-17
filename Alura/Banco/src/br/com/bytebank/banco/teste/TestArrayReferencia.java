9a4943bfb17c1fe83fdb1d4e4857497e54defcb5
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-17 18:43:14 -0300

    Usando referencias

--- Alura/Banco/src/br/com/bytebank/banco/teste/TestArrayReferencia.java
+++ Alura/Banco/src/br/com/bytebank/banco/teste/TestArrayReferencia.java
@@ -1,32 +1,32 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TestArrayReferencia {
     public static void main(String[] args) {
 
-        ContaCorrente[] conta = new ContaCorrente[5];
+        Object[] referencias = new Object[5];
 
         ContaCorrente cc1 = new ContaCorrente(111,111);
+        referencias[0] = cc1;
 
-        conta[0] = cc1;
+        ContaPoupanca cc2 = new ContaPoupanca(111,112);
+        referencias[1] = cc2;
+
+        Cliente cliente = new Cliente();
+        referencias[2] = cliente;
+
+        //System.out.println(referencias[1].getNumero());
+
+        Object referenciaGenerica = referencias[1];
 
 
-        ContaCorrente cc2 = new ContaCorrente(111,112);
-        conta[1] = cc2;
-
-        System.out.println(conta[1].getNumero());
-
-        ContaCorrente ref = conta[1];
+        ContaPoupanca ref = (ContaPoupanca) referencias[1]; //type cast
 
         System.out.println(ref.getNumero());
-
         System.out.println(cc2.getNumero());
-
-
-
-
-
     }
 }
