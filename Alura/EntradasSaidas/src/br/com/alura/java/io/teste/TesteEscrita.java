d8634655511e5c4e70d5d4808774da1d2f82f6b8
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-19 17:24:49 -0300

    Escrevendo um arquivo txt pelo java.io

---
+++ Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteEscrita.java
@@ -0,0 +1,24 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteEscrita {
+    public static void main(String[] args) throws IOException {
+
+        //Fluxo de saida com arquivo
+
+        OutputStream fos = new FileOutputStream("saida.txt");
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        bw.write("1234567890");
+        bw.newLine();
+        bw.newLine();
+        bw.write("1234567890");
+
+
+        bw.close();
+
+
+    }
+}
