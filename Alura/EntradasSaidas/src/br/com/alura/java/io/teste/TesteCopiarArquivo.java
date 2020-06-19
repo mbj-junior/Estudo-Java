f8c2cac1cb4f23e27cdebb8575898f7725897974
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-19 17:35:15 -0300

    Copiando o conteudo de um arquivo para outro

--- Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
+++ Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -2,23 +2,28 @@ package br.com.alura.java.io.teste;
 
 import java.io.*;
 
-public class TesteLeitura {
+public class TesteCopiarArquivo {
     public static void main(String[] args) throws IOException {
 
-        //Fluxo de entrada com arquivo
-
+        //Fluxo de entrada e saida com arquivo
         InputStream fis = new FileInputStream("entrada.txt");
         Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
+        OutputStream fos = new FileOutputStream("saida.txt");
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
         String linha = br.readLine();
 
-        while(linha != null){
-            System.out.println(linha);
+        while (linha != null){
+            bw.write(linha);
+            bw.newLine();
             linha = br.readLine();
         }
 
         br.close();
+        bw.close();
 
 
     }
