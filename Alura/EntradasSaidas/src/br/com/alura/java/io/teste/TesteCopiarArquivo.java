737ca160840ab8a939032e8995272e0a56a07551
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-19 18:26:11 -0300

    Trabalhando com entradas e saida dinamicas

--- Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
+++ Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -1,24 +1,27 @@
 package br.com.alura.java.io.teste;
 
 import java.io.*;
+import java.net.Socket;
 
 public class TesteCopiarArquivo {
     public static void main(String[] args) throws IOException {
+        Socket s new Socket()
 
         //Fluxo de entrada e saida com arquivo
-        InputStream fis = new FileInputStream("entrada.txt");
+        InputStream fis = System.in; //new FileInputStream("entrada.txt");
         Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
-        OutputStream fos = new FileOutputStream("saida.txt");
+        OutputStream fos = System.out;//new FileOutputStream("saida.txt");
         Writer osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
 
         String linha = br.readLine();
 
-        while (linha != null){
+        while (linha != null && !linha.isEmpty()){
             bw.write(linha);
             bw.newLine();
+            bw.flush();
             linha = br.readLine();
         }
 
