c31f2a59d813c4327a3f34b03eefaf499122ff5d
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-19 21:55:00 -0300

    Trabalhando com Scanner e Formatação

--- Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteLeituraScanner.java
+++ Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteLeituraScanner.java
@@ -2,6 +2,7 @@ package br.com.alura.java.io.teste;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeituraScanner {
@@ -11,7 +12,25 @@ public class TesteLeituraScanner {
 
         while (scanner.hasNextLine()){
             String linha = scanner.nextLine();
-            System.out.println(linha);
+            //System.out.println(linha);
+
+            Scanner linhaScanner = new Scanner(linha);
+            linhaScanner.useLocale(Locale.US);
+            linhaScanner.useDelimiter(",");
+
+            String tipoDeConta = linhaScanner.next();
+            int agencia = linhaScanner.nextInt();
+            int conta = linhaScanner.nextInt();
+            String nometitular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
+
+            String valorFormatado = String.format("%s - %04d : %05d - %s $%.2f",tipoDeConta,agencia,conta,nometitular,saldo);
+            System.out.println(valorFormatado);
+
+            linhaScanner.close();
+
+          // String[] valores = linha.split(",");
+           // System.out.println(valores[3]);
         }
         scanner.close();
 
