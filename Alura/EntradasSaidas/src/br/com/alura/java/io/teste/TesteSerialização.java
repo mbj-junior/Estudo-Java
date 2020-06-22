be5bae6640c191ed3ac63d6ff8399f496447c354
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-22 18:21:11 -0300

    Usando Serialização com objetos e trabalhando com versionamento

--- Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteSerialização.java
+++ Alura/EntradasSaidas/src/br/com/alura/java/io/teste/TesteSerialização.java
@@ -4,16 +4,23 @@ import java.io.*;
 
 public class TesteSerialização {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        String nomeOut = "Junior";
+        //String nomeOut = "Junior";
 
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("object.bin"));
-        oos.writeObject(nomeOut);
+        Cliente clienteOut = new Cliente();
+        clienteOut.setNome("Junior");
+        clienteOut.setProfissao("Estudante");
+        clienteOut.setCpf("123123123");
+
+        //Serialização
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
+        oos.writeObject(clienteOut);
         oos.close();
 
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("object.bin"));
-        String nomeIn =(String) ois.readObject();
+        //Deserialização
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.bin"));
+        Cliente clienteIn =(Cliente) ois.readObject();
         ois.close();
-        System.out.println(nomeIn);
+        System.out.println(clienteIn.getNome());
 
     }
 }
