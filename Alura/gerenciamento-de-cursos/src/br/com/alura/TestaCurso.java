ec57e0562c16226e6de4a5cc7dbbc59b354f4468
mBotelho <mbj_junior@hotmail.com>
2020-07-15 22:35:28 -0300

    Comparando linkedList com ArrayList

--- Alura/gerenciamento-de-cursos/src/br/com/alura/TestaCurso.java
+++ Alura/gerenciamento-de-cursos/src/br/com/alura/TestaCurso.java
@@ -1,20 +1,16 @@
 package br.com.alura;
 
-import java.util.List;
-
 public class TestaCurso {
 
 	public static void main(String[] args) {
 
 		Curso javaColecoes = new Curso("Curso1", "Junior");
-		List<Aula> aulas = javaColecoes.getAulas();
-		System.out.println(aulas);
 
-		javaColecoes.getAulas().add(new Aula("Trabalhando", 33));
+		javaColecoes.adiciona(new Aula("Aula1", 11));
+		javaColecoes.adiciona(new Aula("Aula2", 22));
+		javaColecoes.adiciona(new Aula("Aula3", 33));
 
-		System.out.println(aulas);
 		System.out.println(javaColecoes.getAulas());
-		System.out.println(aulas == javaColecoes.getAulas());
 
 	}
 
